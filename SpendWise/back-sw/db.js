cdb623c093445e55df18a251c962ee9720ba0745
mstycone <msmagicone@gmail.com>
2024-11-08 22:10:54 +0100

    MàJ

--- SpendWise/back-sw/db.js
+++ SpendWise/back-sw/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:/', {
+        await mongoose.connect('mongodb://localhost:27017/sw-db', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
--- SpendWise/back-sw/index.js
+++ SpendWise/back-sw/index.js
@@ -9,10 +9,6 @@ connectDB();
 //middleware et routes 
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.listen(port, () => {
   console.log(`Le serveur écoute sur le port : ${port}!`);
 });
