4c8bd8058ff91051e4386aeef30bb971183b0494
mstycone <msmagicone@gmail.com>
2024-11-21 11:02:29 +0100

    Format date moment.js

--- SpendWise/controllers/transactions.js
+++ SpendWise/controllers/transactions.js
@@ -1,9 +1,17 @@
 const Transaction = require('../models/transactions'); 
+const moment = require('moment'); //load module moment
 
 //Récuperer les transactions 
 exports.getAllTransactions = async (req, res) => {
     try {
         const transactions = await Transaction.find();
+
+        //Formater les dates avant envoie au client 
+        const formattedTransactions = transactions.map(transaction => {
+            transaction.date = moment(transaction.date).format('YYYY-MM-DD');
+            return transaction;
+        });
+
         res.json(transactions);
     } catch (error) {
         res.status(500).json({ message: error.message});
@@ -13,8 +21,12 @@ exports.getAllTransactions = async (req, res) => {
 //Ajout d'une nouvelle transaction 
 exports.addTransaction = async (req, res) => {
     try {
-        const { type, categorie, montant } = req.body;
-        const newTransaction = new Transaction({ type, categorie, montant, date: new Date() });
+        const { type, categorie, montant, date } = req.body;
+
+        //Formatage de la date en DD-MM-YYYY
+        const formattedDate = moment(date, 'YYYY-MM-DD').startOf('day').toDate();
+        
+        const newTransaction = new Transaction({ type, categorie, montant, date: formattedDate });
         await newTransaction.save();
         res.status(201).json(newTransaction);
     } catch (error) {
@@ -39,14 +51,18 @@ exports.deleteTransaction = async (req, res) => {
 exports.updateTransaction = async (req, res) => {
     try {
         const { id } = req.params;
-        const { type, categorie, montant } = req.body;
+        const { type, categorie, montant, date } = req.body;
         //Vérifier si la transaction existe déjà 
         const existTransaction = await Transaction.findById(id);
         if (!existTransaction) {
             res.status(404).json({ message: 'Transaction non trouvée' });
         }
+
+        //Formatage de la date en DD-MM-YYYY
+        const formattedDate = moment(date).format('YYYY-MM-DD');
+
         //Mettre à jour la transaction
-        const updatedTransaction = await Transaction.findByIdAndUpdate(id, { type, categorie, montant }, { new: true });
+        const updatedTransaction = await Transaction.findByIdAndUpdate(id, { type, categorie, montant, date: formattedDate }, { new: true });
         res.json(updatedTransaction);
     } catch (error) {
         console.error(`Erreur lors de l'update de la transaction : ${error}`)
